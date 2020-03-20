8585bc9d8fe1b66fb7c88a358e37a4d3f328a768
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-03-20 13:50:02 +0900

    Update code

--- tag_recognition.R
+++ tag_recognition.R
@@ -7,40 +7,66 @@ require('keras')
 
 
 # data load
-data = fread('d:/train.txt', header = T, fill = T)[, c(1, 4)]
+data = fread('d:/train.txt', header = T, fill = T)[, c(1, 4)] %>% 
+  filter(O != '', `-DOCSTART-` != '-DOCSTART-')
 
 # tokenizer
-tokenizer = text_tokenizer(3000, oov_token = 'OOV') %>% 
+tokenizer = text_tokenizer(5000, oov_token = 'OOV') %>% 
   fit_text_tokenizer(data$`-DOCSTART-`)
 
-tag_tokenizer = text_tokenizer(10) %>% 
+tag_tokenizer = text_tokenizer(9,
+                               filters = "!\"#$%&()*+,./:;<=>?@[\\]^_`{|}~\t\n") %>% 
   fit_text_tokenizer(data$O)
 
-# data split
+# data preprocess
+tag = data$O %>% unique()
+tag = seq_along(tag) %>% setNames(tag)
+
 ind = sample(2, nrow(data), replace = T, prob = c(0.7, 0.3))
-x_train = tokenizer %>% texts_to_sequences(data$`-DOCSTART-`) %>% filter(ind == 1)
-y_train = tokenizer %>% texts_to_sequences(data$O) %>% filter(ind == 2)
+input_sequence = tokenizer %>% texts_to_sequences(data$`-DOCSTART-`) %>% 
+  pad_sequences(padding = 'post', maxlen = 7)
+tag_sequence = tag_tokenizer %>% texts_to_sequences(data$O) %>% 
+  pad_sequences(padding = 'post', maxlen = 7) %>% 
+  to_categorical(num_classes = tag_tokenizer$num_words)
+
+c(x_train, y_train) %<-% list(input_sequence[ind == 1, ],
+                              tag_sequence[ind == 1, ,])
+
+c(x_test, y_test) %<-% list(input_sequence[ind == 2, ],
+                            tag_sequence[ind == 2, ,])
   
   
   
   
 # model
 model = keras_model_sequential() %>% 
-  layer_embedding() %>% 
-  layer_lstm(units = 256, 
-             return_sequences = T) %>% 
-  bidirectional() %>% 
-  layer_dense(units = 10,
-              activation = 'softmax') %>% 
-  time_distributed() %>% 
+  layer_embedding(input_dim = 5000, 
+                  output_dim = 256, 
+                  input_length = 7,
+                  mask_zero = T) %>% 
+  bidirectional(
+    layer_lstm(units = 256, 
+               return_sequences = T)) %>% 
+  time_distributed(
+    layer_dense(units = 9,
+                activation = 'softmax')
+    ) %>% 
   # compile
-  compile(loss = 'categorical_crossenttropy',
-          optimizer = Nadam(0.001),
+  compile(loss = 'categorical_crossentropy',
+          optimizer = 'Nadam',
           metric = 'accuracy')
 
+# train
 history = model %>% 
   fit(x_train,
-      y_tran,
+      y_train,
       batch_size = 128,
       epochs = 50,
-      validation_data = 0.3)
+      validation_data = list(x_test, y_test),
+      callbacks = list(
+        callback_early_stopping(monitor = 'val_loss',
+                                mode = 'min',
+                                verbose = 1,
+                                patience = 7)
+        )
+  )

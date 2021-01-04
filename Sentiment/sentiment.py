3660ab4ff50e1747799707857bea2ee6e03a8d49
Jihwan Min <55495983+rluver@users.noreply.github.com>
2021-01-04 23:37:18 +0900

    feat: Create sentiment.py

---
+++ Sentiment/sentiment.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import pandas as pd
+import os
+
+
+labels = ['0', '1']
+
+
+def loadData(path):
+    '''    
+
+    Parameters
+    ----------
+    path : directory
+        path where data exist.
+
+    Returns
+    -------
+    news : dataframe
+        dataframe including news data.
+
+    '''
+    news = pd.DataFrame()
+    
+    for filename in os.listdir(path):
+        news = news.append(
+            pd.read_excel(
+                os.path.join(path, filename), 
+                header = None), 
+            ignore_index = True
+            )
+    
+    news.columns = news.iloc[0]
+    news = news.iloc[1:].reset_index(drop = True)
+    
+    return news

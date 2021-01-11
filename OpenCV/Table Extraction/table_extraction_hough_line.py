5a79d74c611c45c99afa23b63abf4f8c85fbde4c
Jihwan Min <55495983+rluver@users.noreply.github.com>
2021-01-11 23:42:25 +0900

    style: Edit table_extraction_hough_line.py to insert space

--- OpenCV/Table Extraction/table_extraction_hough_line.py
+++ OpenCV/Table Extraction/table_extraction_hough_line.py
@@ -1,43 +1,46 @@
 import cv2 as cv
 import numpy as np
 
+
+
+
 def getFile(file):
   
-  image = cv.imread(cv.samples.findFile(file))
+    image = cv.imread(cv.samples.findFile(file))
   
-  return image
+      return image
   
   
 def showImage(function):
-  def wrapper(image):
-    image = function(image)
-    cv.imshow('image', image)
-    cv.waitKey(0)
-    # cv.destroyWindow('image')
+    def wrapper(image):
+        image = function(image)
+        cv.imshow('image', image)
+        cv.waitKey(0)
+        # cv.destroyWindow('image')
 
 
 @showImage
 def showGrayImage(image):
   
-  gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
   
-  return gray_image
+      return gray_image
   
   
 @showImage
 def showCannyImage(gray_image):
   
-  canny_image = cv.Canny(gray_image, 50, 150)
+    canny_image = cv.Canny(gray_image, 50, 150)
   
-  return canny_image
+      return canny_image
   
   
   
   
 if __name__ == '__main__':
-  file = 'd:/source.png'
-  image = getFile(file)
-  image_copy = np.copy(image)
+    file = 'd:/source.png'
+    image = getFile(file)
+    image_copy = np.copy(image)
   
-  showGrayImage(image)
-  showCannyImage(image)
+    showGrayImage(image)
+    showCannyImage(image)

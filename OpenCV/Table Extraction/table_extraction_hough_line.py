018cf8a360ce5ee5409db995dd8d6371e373276d
Jihwan Min <55495983+rluver@users.noreply.github.com>
2021-01-11 23:40:19 +0900

    feat: Upload table_extraction_hough_line.py

---
+++ OpenCV/Table Extraction/table_extraction_hough_line.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import numpy as np
+
+def getFile(file):
+  
+  image = cv.imread(cv.samples.findFile(file))
+  
+  return image
+  
+  
+def showImage(function):
+  def wrapper(image):
+    image = function(image)
+    cv.imshow('image', image)
+    cv.waitKey(0)
+    # cv.destroyWindow('image')
+
+
+@showImage
+def showGrayImage(image):
+  
+  gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+  
+  return gray_image
+  
+  
+@showImage
+def showCannyImage(gray_image):
+  
+  canny_image = cv.Canny(gray_image, 50, 150)
+  
+  return canny_image
+  
+  
+  
+  
+if __name__ == '__main__':
+  file = 'd:/source.png'
+  image = getFile(file)
+  image_copy = np.copy(image)
+  
+  showGrayImage(image)
+  showCannyImage(image)

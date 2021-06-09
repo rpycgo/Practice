a1a4bcd6b5af057ed834577588bf90e27693fa69
rluver <skyd0608@gmail.com>
2021-06-09 10:34:09 +0900

    feat: EDit crnn_ocr.py to apply multi head attention

--- Computer Vision/crnn_ocr.py
+++ Computer Vision/crnn_ocr.py
@@ -1,3 +1,5 @@
+from layers.multi_head_attention import MultiHeadAttention
+
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,113 +20,9 @@ from tensorflow.keras.layers import (
     BatchNormalization, 
     Lambda, 
     Bidirectional, 
-    LSTM
+    LSTM,
+    Concatenate
     )
-
-
-
-######### data #########s
-#!curl -L0 https://github.com/AakashKumarNain/CaptchaCracker/raw/master/captcha_images_v2.zip
-#!unzip -qq captcha_images_v2.zip
-
-
-
-data_dir = Path('./captcha_images_v2/')
-images = sorted(list(map(str, list(data_dir.glob("*.png")))))
-labels = [img.split(os.path.sep)[-1].split(".png")[0] for img in images]
-characters = set(char for label in labels for char in label)
-
-print("Number of images found: ", len(images))
-print("Number of labels found: ", len(labels))
-print("Number of unique characters: ", len(characters))
-print("Characters present: ", characters)
-
-downsample_factor = 4
-max_length = max([len(label) for label in labels])
-
-
-# preprocessing
-char_to_num = layers.experimental.preprocessing.StringLookup(
-    vocabulary = list(characters), num_oov_indices = 0, mask_token = None
-)
-
-num_to_char = layers.experimental.preprocessing.StringLookup(
-    vocabulary=char_to_num.get_vocabulary(), mask_token = None, invert = True
-)
-
-
-
-def split_data(images, labels, train_size = 0.9, shuffle = True):
-    # 1. Get the total size of the dataset
-    size = len(images)
-    # 2. Make an indices array and shuffle it, if required
-    indices = np.arange(size)
-    if shuffle:
-        np.random.shuffle(indices)
-    # 3. Get the size of training samples
-    train_samples = int(size * train_size)
-    # 4. Split data into training and validation sets
-    x_train, y_train = images[indices[:train_samples]], labels[indices[:train_samples]]
-    x_valid, y_valid = images[indices[train_samples:]], labels[indices[train_samples:]]
-    return x_train, x_valid, y_train, y_valid
-
-
-# Splitting data into training and validation sets
-x_train, x_valid, y_train, y_valid = split_data(np.array(images), np.array(labels))
-
-
-def encode_single_sample(img_path, label):
-    # 1. Read image
-    img = tf.io.read_file(img_path)
-    # 2. Decode and convert to grayscale
-    img = tf.io.decode_png(img, channels = 1)
-    # 3. Convert to float32 in [0, 1] range
-    img = tf.image.convert_image_dtype(img, tf.float32)
-    # 4. Resize to the desired size
-    img = tf.image.resize(img, [img_height, img_width])
-    # 5. Transpose the image because we want the time
-    # dimension to correspond to the width of the image.
-    img = tf.transpose(img, perm = [1, 0, 2])
-    # 6. Map the characters in label to numbers
-    label = char_to_num(tf.strings.unicode_split(label, input_encoding = 'UTF-8'))
-    # 7. Return a dict as our model is expecting two inputs
-    return {'input_layer': img, 'label_input': label}
-
-
-
-train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_dataset = (
-    train_dataset.map(
-        encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
-    .batch(batch_size)
-    .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-)
-
-validation_dataset = tf.data.Dataset.from_tensor_slices((x_valid, y_valid))
-validation_dataset = (
-    validation_dataset.map(
-        encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
-    .batch(batch_size)
-    .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-)
-
-
-
-
-# visualize data
-_, ax = plt.subplots(4, 4, figsize = (10, 5))
-for batch in train_dataset.take(1):
-    images = batch['input_layer']
-    labels = batch['label_input']
-    for i in range(16):
-        img = (images[i] * 255).numpy().astype('uint8')
-        label = tf.strings.reduce_join(num_to_char(labels[i])).numpy().decode('utf-8')
-        ax[i // 4, i % 4].imshow(img[:, :, 0].T, cmap = 'gray')
-        ax[i // 4, i % 4].set_title(label)
-        ax[i // 4, i % 4].axis('off')
-plt.show()
 
 
 
@@ -155,23 +53,28 @@ class CTCLayer(layers.Layer):
 
 
 
+
 class CRNNOCR:
     
-    def __init__(self):
+    def __init__(self, max_char_len, image_height, image_width):
         
-        self.max_char_len = 19
+        self.max_char_len = max_char_len
+        self.image_height = image_height
+        self.image_width = image_width
         
     
     def ctc_lambda_function(self, args):
+        
         labels, y_pred, input_length, label_length = args
         
         return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
     
   
     def build_model(self):
-        
-        inputs = Input(shape = (32, 128, 1), name = 'input_layer')
+
+        inputs = Input(shape = (self.image_height, self.image_width, 1), name = 'input_layer')
         labels = Input(shape = (None, ), name = 'label_input', dtype = 'float32')
+        #padding_mask = Input(shape=(1, 1, None), name = 'padding_mask')
         
         convolution_layer_1 = Conv2D(
             filters = 64,
@@ -276,34 +179,230 @@ class CRNNOCR:
           
         
         
-        reshape_layer = Reshape((-1, 512), name = 'reshape_layer')(convolution_layer_6)
+        # reshape_layer = Reshape((-1, 512), name = 'reshape_layer')(convolution_layer_6)
         
                   
-        ######################################################################################################
+        # ######################################################################################################
   
         
-        bidirectional_lstm_layer_1 = Bidirectional(
-            LSTM(
-              units = 256,
-              return_sequences = True,
-              dropout = 0.2,
-              name = 'bilstm_layer_1')
-            )(reshape_layer)
+        # bidirectional_lstm_layer_1 = Bidirectional(
+        #     LSTM(
+        #       units = 256,
+        #       return_sequences = True,
+        #       dropout = 0.2,
+        #       name = 'bilstm_layer_1')
+        #     )(reshape_layer)
           
-        bidirectional_lstm_layer_2 = Bidirectional(
-            LSTM(
-              units = 256,
-              return_sequences = True,
-              dropout = 0.2,
-              name = 'bilstm_layer_2')
-            )(bidirectional_lstm_layer_1)
+        # bidirectional_lstm_layer_2 = Bidirectional(
+        #     LSTM(
+        #       units = 256,
+        #       return_sequences = True,
+        #       #return_state = True,
+        #       dropout = 0.2,
+        #       name = 'bilstm_layer_2')
+        #     )(bidirectional_lstm_layer_1)
+        
+        multi_head_attention_layer = MultiHeadAttention(d_model = 512, num_heads = 8, name = 'multi_head_attention_layer')(
+            {'query': convolution_layer_6,
+             'key': convolution_layer_6,
+             'value': convolution_layer_6
+             }
+            )
         
         
-        outputs = Dense(units = self.max_char_len + 1, activation = 'softmax', name = 'classification_layer_1')(bidirectional_lstm_layer_2)
+        outputs = Dense(units = self.max_char_len + 1, activation = 'softmax', name = 'classification_layer')(multi_head_attention_layer)
         
         ctc_loss = CTCLayer(name = 'ctc_loss')(labels, outputs)
         
         model = Model(inputs = [inputs, labels], outputs = ctc_loss)
         
         return model
+    
+    
+    
+def split_data(images, labels, train_size = 0.9, shuffle = True):    
+    # 1. Get the total size of the dataset
+    size = len(images)
+    # 2. Make an indices array and shuffle it, if required
+    indices = np.arange(size)
+    if shuffle:
+        np.random.shuffle(indices)
+    # 3. Get the size of training samples
+    train_samples = int(size * train_size)
+    # 4. Split data into training and validation sets
+    x_train, y_train = images[indices[:train_samples]], labels[indices[:train_samples]]
+    x_valid, y_valid = images[indices[train_samples:]], labels[indices[train_samples:]]
+    return x_train, x_valid, y_train, y_valid
+    
+    
+    # Splitting data into training and validation sets
+    x_train, x_valid, y_train, y_valid = split_data(np.array(images), np.array(labels))
+    
+
+def encode_single_sample(img_path, label):
+    # 1. Read image
+    img = tf.io.read_file(img_path)
+    # 2. Decode and convert to grayscale
+    img = tf.io.decode_png(img, channels = 1)
+    # 3. Convert to float32 in [0, 1] range
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # 4. Resize to the desired size
+    img = tf.image.resize(img, [IMAGE_HEIGHT, IMAGE_WIDTH])
+    # 5. Transpose the image because we want the time
+    # dimension to correspond to the width of the image.
+    img = tf.transpose(img, perm = [1, 0, 2])
+    # 6. Map the characters in label to numbers
+    label = char_to_num(tf.strings.unicode_split(label, input_encoding = 'UTF-8'))
+    # 7. Return a dict as our model is expecting two inputs
+    return {'input_layer': img, 'label_input': label}
+
+
+
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = K.ctc_decode(pred, input_length = input_len, greedy = True)[0][0][
+        :, :max_length
+    ]
+    
+    output_text = []
+    for res in results:
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode('utf-8')
+        output_text.append(res)
         
+    return output_text
+    
+
+    
+    
+if __name__ == '__main__':
+    
+    ######### data #########s
+    #!curl -L0 https://github.com/AakashKumarNain/CaptchaCracker/raw/master/captcha_images_v2.zip
+    #!unzip -qq captcha_images_v2.zip
+        
+    data_dir = Path('./captcha_images_v2/')
+    images = sorted(list(map(str, list(data_dir.glob('*.png')))))
+    labels = [img.split(os.path.sep)[-1].split(".png")[0] for img in images]
+    characters = set(char for label in labels for char in label)
+    
+    print('Number of images found: ', len(images))
+    print('Number of labels found: ', len(labels))
+    print('Number of unique characters: ', len(characters))
+    print('Characters present: ', characters)
+    
+    downsample_factor = 4
+    max_length = max([len(label) for label in labels])
+    BATCH_SIZE = 8
+    
+    IMAGE_HEIGHT = 200
+    IMAGE_WIDTH = 50
+    
+    # preprocessing
+    char_to_num = layers.experimental.preprocessing.StringLookup(
+        vocabulary = list(characters), num_oov_indices = 0, mask_token = None
+    )
+    
+    num_to_char = layers.experimental.preprocessing.StringLookup(
+        vocabulary=char_to_num.get_vocabulary(), mask_token = None, invert = True
+    )
+    
+    
+    
+    # split data
+    x_train, x_valid, y_train, y_valid = split_data(np.array(images), np.array(labels))
+    
+    
+    # tensorflow dataset
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_dataset = (
+        train_dataset.map(
+            encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+        .batch(BATCH_SIZE)
+        .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    )
+    
+    validation_dataset = tf.data.Dataset.from_tensor_slices((x_valid, y_valid))
+    validation_dataset = (
+        validation_dataset.map(
+            encode_single_sample, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+        .batch(BATCH_SIZE)
+        .prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    )
+    
+    
+    # visualize data
+    _, ax = plt.subplots(4, 4, figsize = (10, 5))
+    for batch in train_dataset.take(1):
+        images = batch['input_layer']
+        labels = batch['label_input']
+        for i in range(16):
+            img = (images[i] * 255).numpy().astype('uint8')
+            label = tf.strings.reduce_join(num_to_char(labels[i])).numpy().decode('utf-8')
+            ax[i // 4, i % 4].imshow(img[:, :, 0].T, cmap = 'gray')
+            ax[i // 4, i % 4].set_title(label)
+            ax[i // 4, i % 4].axis('off')
+    plt.show()
+    
+    
+    
+    model = CRNNOCR(19, IMAGE_HEIGHT, IMAGE_WIDTH).build_model()
+    model.summary()
+    EPOCHS = 100
+    EARLY_STOPPING_PATIENCE = 10
+    
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor = 'val_loss',
+        patience = EARLY_STOPPING_PATIENCE,
+        restore_best_weights = True
+        )
+    
+    history = model.fit(
+        train_dataset,
+        validation_ata = validation_dataset,
+        epochs = EPOCHS,
+        callbacks = [early_stopping]
+        )
+    
+    
+    # prediction model
+    prediction_model = tf.keras.models.Model(
+        model.get_layer(name = 'input_layer').input,
+        model.get_layer(name = 'classification_layer').output
+        )
+    
+    prediction_model.summary()
+    
+    
+    # predicted value visualization
+    for batch in validation_dataset.take(1):
+        batch_images = batch['input_layer']
+        batch_labels = batch['label_input']
+    
+        preds = prediction_model.predict(batch_images)
+        pred_texts = decode_batch_predictions(preds)
+    
+        orig_texts = []
+        for label in batch_labels:
+            label = tf.strings.reduce_join(num_to_char(label)).numpy().decode('utf-8')
+            orig_texts.append(label)
+    
+        _, ax = plt.subplots(4, 4, figsize=(15, 5))
+        for i in range(len(pred_texts)):
+            img = (batch_images[i, :, :, 0] * 255).numpy().astype(np.uint8)
+            img = img.T
+            title = f'Prediction: {pred_texts[i]}'
+            ax[i // 4, i % 4].imshow(img, cmap = 'gray')
+            ax[i // 4, i % 4].set_title(title)
+            ax[i // 4, i % 4].axis('off')
+            
+    plt.show()
+        
+        
+        
+    
+    
+    
+    

7ced5b9c285b67d4b007e1a8b20e1815b34b5df2
rluver <skyd0608@gmail.com>
2021-06-09 10:33:23 +0900

    create: Upload multi_head_attention.py

---
+++ Computer Vision/layers/multi_head_attention.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  9 09:06:39 2021
+
+@author: MJH
+"""
+from layers.scaled_dot_product_attention import scaled_dot_product_attention
+
+import tensorflow as tf
+
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+  def __init__(self, d_model, num_heads, name="multi_head_attention"):
+    super(MultiHeadAttention, self).__init__(name=name)
+    self.num_heads = num_heads
+    self.d_model = d_model
+
+    assert d_model % self.num_heads == 0
+
+    # d_model을 num_heads로 나눈 값.
+    # 논문 기준 : 64
+    self.depth = d_model // self.num_heads
+
+    # WQ, WK, WV에 해당하는 밀집층 정의
+    self.query_dense = tf.keras.layers.Dense(units=d_model)
+    self.key_dense = tf.keras.layers.Dense(units=d_model)
+    self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+    # WO에 해당하는 밀집층 정의
+    self.dense = tf.keras.layers.Dense(units=d_model)
+
+  # num_heads 개수만큼 q, k, v를 split하는 함수
+  def split_heads(self, inputs, batch_size):
+    inputs = tf.reshape(
+        inputs, shape=(batch_size, -1, self.num_heads, self.depth))
+    return tf.transpose(inputs, perm=[0, 2, 1, 3])
+
+  def call(self, inputs):
+    #query, key, value, mask = inputs['query'], inputs['key'], inputs['value'], inputs['mask']
+    #batch_size = tf.shape(query)[0]
+      
+    query, key, value = inputs['query'], inputs['key'], inputs[
+        'value']#, inputs['mask']
+    batch_size = tf.shape(query)[0]
+    
+
+    # 1. WQ, WK, WV에 해당하는 밀집층 지나기
+    # q : (batch_size, query의 문장 길이, d_model)
+    # k : (batch_size, key의 문장 길이, d_model)
+    # v : (batch_size, value의 문장 길이, d_model)
+    # 참고) 인코더(k, v)-디코더(q) 어텐션에서는 query 길이와 key, value의 길이는 다를 수 있다.
+    query = self.query_dense(query)
+    key = self.key_dense(key)
+    value = self.value_dense(value)
+
+    # 2. 헤드 나누기
+    # q : (batch_size, num_heads, query의 문장 길이, d_model/num_heads)
+    # k : (batch_size, num_heads, key의 문장 길이, d_model/num_heads)
+    # v : (batch_size, num_heads, value의 문장 길이, d_model/num_heads)
+    query = self.split_heads(query, batch_size)
+    key = self.split_heads(key, batch_size)
+    value = self.split_heads(value, batch_size)
+
+    # 3. 스케일드 닷 프로덕트 어텐션. 앞서 구현한 함수 사용.
+    # (batch_size, num_heads, query의 문장 길이, d_model/num_heads)
+    scaled_attention, _ = scaled_dot_product_attention(query, key, value)
+    # (batch_size, query의 문장 길이, num_heads, d_model/num_heads)
+    scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+    # 4. 헤드 연결(concatenate)하기
+    # (batch_size, query의 문장 길이, d_model)
+    concat_attention = tf.reshape(scaled_attention,
+                                  (batch_size, -1, self.d_model))
+
+    # 5. WO에 해당하는 밀집층 지나기
+    # (batch_size, query의 문장 길이, d_model)
+    outputs = self.dense(concat_attention)
+
+    return outputs

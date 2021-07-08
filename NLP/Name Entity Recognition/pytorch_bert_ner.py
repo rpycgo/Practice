06e4b6f56b42e620ff6a878d6657f7bda0f8f188
Jihwan Min <55495983+rluver@users.noreply.github.com>
2021-07-08 23:14:50 +0900

    feat: Edit pytorch_bert_ner.py to include get_input_data function

--- NLP/Name Entity Recognition/pytorch_bert_ner.py
+++ NLP/Name Entity Recognition/pytorch_bert_ner.py
@@ -45,3 +45,53 @@ def load_data(file_path, sep = '\t'):
                 dictionary.append(temp)
                 
     return dictionary
+
+
+def get_input_data(dataset):
+    
+    input_dataframe = pd.DataFrame(columns = ['text', 'tags'])
+    
+    for data in dataset:
+        sentence = data['sentence']
+        sentence = sentence.replace('\xad', '­＿')
+        name_entity = data['ne']
+        
+        if name_entity == {}:
+            continue
+        
+        bert_tokenized_sentence = tokenizer.wordpiece_tokenizer.tokenize(sentence)
+        sentence = bert_tokenized_sentence
+        
+        character_dataframe = pd.DataFrame([j for i in sentence for j in i], columns = ['text'])
+        
+        for key in name_entity.keys():
+            for find in re.finditer(key, ''.join(sentence)):
+                index = find.span()
+                if ( index[1] - index[0] ) == 1:
+                    character_dataframe.loc[index[0], 'tag'] = 'B-' + name_entity[key]
+                else:
+                    character_dataframe.loc[index[0], 'tag'] = 'B-' + name_entity[key]
+                    character_dataframe.loc[( index[0] + 1 ) : (index[1] - 1 ), 'tag'] = 'I-' + name_entity[key]
+            
+        character_dataframe.fillna('O', inplace = True)
+        
+        start = 0
+        bert_tag_list = []
+        for token in bert_tokenized_sentence:
+            bert_tag_list.append((token, start, len(token)))
+            start += len(token)
+        
+        try:
+            temp_dict = [{'name': row[0], 'tag': character_dataframe.iloc[row[1]].tag} for row in bert_tag_list]
+        except:
+            continue
+        
+        bert_tag = list(map(lambda x: x['tag'], temp_dict))
+        
+        input_dataframe = input_dataframe.append(pd.DataFrame([[sentence, bert_tag]], columns = ['text', 'tags']))
+        
+        input_dataframe = input_dataframe[input_dataframe.text.map(len) <= 98]
+        input_dataframe = input_dataframe[input_dataframe.text.apply(lambda x: max([len(i) for i in x]))  <= 18]
+        input_dataframe.reset_index(drop = True, inplace = True)
+        
+    return input_dataframe

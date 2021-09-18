2e31f3d2100f1827fcc75aef14fbde697a376d13
Jihwan Min <55495983+rluver@users.noreply.github.com>
2021-09-19 00:43:10 +0900

    fix: Edit 2579.go to change code
    fix:
     - change Min to Max
    refactor:
     - use Fscan

--- Algorithm/BOJ/Go/Dynamic Programming/2579.go
+++ Algorithm/BOJ/Go/Dynamic Programming/2579.go
@@ -2,26 +2,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
 )
+
 
 func solution(stairs []int, n int) int {
 
-	answer := []int{stairs[0]}
+	answer := make([]int, n, n)
+	answer[0] = stairs[0]
+	
 
 	for i := 1; i < n; i++ {
 		if i == 1 {
-			answer = append(answer, (answer[i-1] + stairs[1]))
+			answer[i] = (answer[i-1] + stairs[1])
 		} else if i == 2 {
-			max := int(math.Min(float64(stairs[i]+stairs[i-1]), float64(stairs[i]+answer[i-2])))
-			answer = append(answer, max)
+			max := int(math.Max(float64(stairs[i]+stairs[i-1]), float64(stairs[i]+answer[i-2])))
+			answer[i] = max
 		} else {
-			max := int(math.Min(float64(stairs[i]+stairs[i-1]+answer[i-3]), float64(stairs[i]+answer[i-2])))
-			answer = append(answer, max)
+			max := int(math.Max(float64(stairs[i]+stairs[i-1]+answer[i-3]), float64(stairs[i]+answer[i-2])))
+			answer[i] = max
 		}
 	}
-
+	
 	return answer[n-1]
 }
 
@@ -29,14 +34,13 @@ func solution(stairs []int, n int) int {
 func main() {
 
 	var n int
-	fmt.Scan(&n)
+	fmt.Scanln(&n)
 
-	stairs := []int{}
+	r := bufio.NewReader(os.Stdin)
+	stairs := make([]int, n, n)
 	for i := 0; i < n; i++ {
-		var input int
-		fmt.Scan(&input)
-		stairs = append(stairs, input)
+		fmt.Fscan(r, &stairs[i])
 	}
-
+	
 	fmt.Println(solution(stairs, n))
 }
